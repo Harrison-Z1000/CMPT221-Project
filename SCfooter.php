6c8f05e817f66e418a965a41bb8fd6bbc4ea2512
Oliver115 <54683415+Oliver115@users.noreply.github.com>
2020-10-28 18:00:41 -0400

    Add files via upload

---
+++ SCfooter.php
@@ -0,0 +1,18 @@
+<!-- Footer for the Sunset Cruises website 
+		Created on 10 October 2020
+-->
+
+<?php
+
+	define("FILE_AUTHOR", "Oliver Wilson");
+	define("TEAM_MEMBERS", "Oliver Wilson, Devin White, Harrison Zheng");
+
+	echo "<footer style='font-size: 12px; text-align: center;'> <small> DISCLAIMER! <br> This Website was made for educational purposes only as a way for the students developing this page to learn"; 
+				echo " the topics of Software Development II. By using this site you understand that no real cruises are being";
+				echo "sold and that all the products on the site are fake. This site should not be used as a substitute for a cruise travel agency.";
+				echo "<br>";
+				echo "2020 (C) " . FILE_AUTHOR . ", United States";
+				echo "<br> This Website is currently at: <a href='Change.php'> V2.0 </a>";
+				echo "<br>  Privacy Policy";
+	echo "</footer>";
+?>
