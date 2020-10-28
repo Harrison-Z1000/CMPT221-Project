f786a5c2865376b30c8543c4b0de5a51a309096d
Devin <60376349+Devinwhite37@users.noreply.github.com>
2020-10-28 18:12:11 -0400

    Update and rename ContactUs.html to ContactUs.php

--- T18/HTML/ContactUs.php
+++ T18/HTML/ContactUs.php
@@ -193,13 +193,10 @@ input[type=submit]:hover {
 
 <hr>
 <!-- Disclaimer -->
-<footer style="font-size: 12px; text-align: center;"> <small> DISCLAIMER! <br> This Website was made for educational purposes only as a way for the students developing this page to learn 
-				 the topics of Software Development II. By using this site you understand that no real cruises are being 
-				 sold and that all the products on the site are fake. This site should not be used as a substitute for a cruise travel agency.
-				 <br> 
-				2020 (C) Oliver Wilson - Harrison Zheng - Devin White, United States
-				<br>  Privacy Policy <!-- Link for Privacy Policy page goes here -->
-	</footer>
-<br>
+	
+<php?
+      include "SCfooter.php";
+?>
+	
 </body>
 </html>
