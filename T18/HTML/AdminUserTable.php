ce34b17bd5acbfab31fe617408193d3b0fa5cf06
Devin <60376349+Devinwhite37@users.noreply.github.com>
2020-11-17 15:47:05 -0500

    Update AdminUserTable.php

--- T18/HTML/AdminUserTable.php
+++ T18/HTML/AdminUserTable.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 
 <!--
-     User table Page
-     Created by Team 18. 27-Oct-2020
+     Cruise(Supplier) table Page
+     Created by Team18 27-Oct-2020
+		 edited 17-Nov-2020
 -->
 
 <!--          Header         -->
@@ -14,10 +15,10 @@
 	<!-- CSS -->
 	<style>
 		body { background: url(Crab.jpg) no-repeat;
-			background-size: cover;
-			height: 78vh;
-			background-position: center;
-			}
+				background-size: cover;
+				height: 100vh;
+				background-position: center;
+				}
 
 		p { margin: 1px;
 			padding: 7px;
@@ -44,15 +45,15 @@
 			  }
 
 		table, td { margin-right:auto;
-			  margin-left: auto;
-			  border:2px solid black;
-			  border-collapse:collapse;
-			  padding: 15px;
-	  		  text-align: left;
-			  background-color: white;
-			  }
+					margin-left: auto;
+					border:2px solid black;
+					border-collapse:collapse;
+					padding: 15px;
+					text-align: left;
+					background-color: white;
+					}
 
-       		th { margin-right:auto;
+        th { margin-right:auto;
 			 margin-left: auto;
 			 border:2px solid black;
 			 border-collapse:collapse;
@@ -61,6 +62,30 @@
 			 background-color: orange;
 			 color:white;
 			 }
+
+		.button {
+			  border: none;
+			  color: white;
+			  padding: 10px 26px;
+			  text-align: center;
+			  text-decoration: none;
+			  display: inline-block;
+			  font-size: 16px;
+			  margin: 4px 2px;
+			  transition-duration: 0.4s;
+			  cursor: pointer;
+			}
+
+		.button1 {
+			  background-color: white;
+			  color: black;
+			  border: 2px solid black;
+			}
+
+		.button1:hover {
+			  background-color: grey;
+			  color: white;
+			}
 
 	</style>
 </head>
@@ -83,9 +108,17 @@
 	/*
 		Query to get Columns from the Users Table
 	*/
-	$table="T18_Users";
+	$table="Team18_Users";
 	$q="EXPLAIN $table";
 	$r=mysqli_query($dbc, $q);
+
+	// Set $sort_type to passed input from the form
+	if (isset($_POST['sort'])) {
+		$sort_type=$_POST['sort'];
+	}
+	else {
+		$sort_type="User_ID";
+	}
 
 	if ($r) {
 		while ($row=mysqli_fetch_array($r, MYSQLI_NUM)) {
@@ -101,7 +134,8 @@
 	/*
 		Query to get Data from Users Table
 	*/
-	$q="SELECT * FROM $table";
+	$q="SELECT * FROM $table
+			ORDER BY $sort_type";
 	$r=mysqli_query($dbc, $q);
 
 		echo "<tr>";
@@ -117,9 +151,29 @@
 		}
 	}
 	echo "</table>";
+
+	/**********************************************************************
+	*	Button to re-run this same file, sort by may be used
+	**********************************************************************/
+
+	echo "<form style='color: white;' action='AdminUserTable.php' method='POST'>";
+		echo "<br> <input type='submit' value='SORT BY:'>";
+		echo "<input type='radio' name='sort' value='User_Id'>			ID";
+		echo "<input type='radio' name='sort' value='User_name'>			Name";
+		echo "<input type='radio' name='sort' value='User_Password'>		Password";
+		echo "<input type='radio' name='sort' value='User_email'>		Email";
+		echo "<input type='radio' name='sort' value='User_active'>		Active?";
+		echo "<input type='radio' name='sort' value='employee_type'>		Type";
+		echo "<input type='radio' name='sort' value='creation_date'>		Date";
+	echo "</form>";
+
+
+	echo "<br><form action='AddRowUser.php'>";
+		echo "<button class='button button1' onclick=> Add a New User </button>";
+	echo "</form>";
 ?>
 
-<br><br><br><br><br><br>
+
 <hr>
 <?php
 	include "SCfooter.php"; // Include the Sunset Cruises Footer
@@ -130,3 +184,4 @@
 </html>
 
 <!-- END OF FILE -->
+
