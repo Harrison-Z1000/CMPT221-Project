32ae08093bf6a832ed669edfee22f3673769ec03
Devin <60376349+Devinwhite37@users.noreply.github.com>
2020-11-18 17:47:44 -0500

    Update AddRowUser.php

--- T18/HTML/AddRowUser.php
+++ T18/HTML/AddRowUser.php
@@ -177,7 +177,7 @@
 		echo "<form style='background-color: rgb(0,0,0,.4); color: white;' action='AddRowUser.php' method='POST'>";
 		echo "<fieldset>";
 			echo "<br> User Name: 						<input type='text' name='username' value=$username>";
-			echo "<br> Password:  			<input type='password' name='password' value='$password'>";
+			echo "<br> Password:  			<input type='password' name='password'>";
 			echo "<br> Email:					<input type='email' name='email' value='$email'>";
 			echo "</select>";
 			echo "<br> Employee type: 		<select name='type'>";
