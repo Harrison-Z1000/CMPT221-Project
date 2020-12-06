5c849a1e230e993cd20a75681e3f346c9944f27c
Devin <60376349+Devinwhite37@users.noreply.github.com>
2020-12-05 20:26:48 -0500

    Update SCfooter.php

--- T18/HTML/SCfooter.php
+++ T18/HTML/SCfooter.php
@@ -8,7 +8,7 @@
 	define("TEAM_MEMBERS", "Oliver Wilson, Devin White, Harrison Zheng");
 
 	echo "<footer style='font-size: 12px; text-align: center; color: grey;'> <small> DISCLAIMER! <br> This Website was made for educational purposes only as a way for the students developing this page to learn"; 
-				echo " the topics of Software Development II. By using this site you understand that no real cruises are being";
+				echo " the topics of Software Development II. By using this site you understand that no real cruises are being ";
 				echo "sold and that all the products on the site are fake. This site should not be used as a substitute for a cruise travel agency.";
 				echo "<br>";
 				echo "2020 (C) " . FILE_AUTHOR . ", United States";
